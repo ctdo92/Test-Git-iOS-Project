946a8b3c5ccbdb3fd250ee0967eddaea4b86413c
Chuong Do <chuongdo@Chuongs-MacBook-Air.local>
2017-05-04 13:32:02 -0500

    added a Test class

---
+++ Git Test/CTDTest.h
@@ -0,0 +1,13 @@
+//
+//  CTDTest.h
+//  Git Test
+//
+//  Created by Chuong Do on 5/4/17.
+//  Copyright © 2017 goehr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CTDTest : NSObject
+
+@end
---
+++ Git Test/CTDTest.m
@@ -0,0 +1,13 @@
+//
+//  CTDTest.m
+//  Git Test
+//
+//  Created by Chuong Do on 5/4/17.
+//  Copyright © 2017 goehr. All rights reserved.
+//
+
+#import "CTDTest.h"
+
+@implementation CTDTest
+
+@end
